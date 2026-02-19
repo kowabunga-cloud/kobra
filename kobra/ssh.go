40d4ea3a7f54af09e5d91d94df24369fc797b39c
Benjamin Zores <benjamin.zores@gmail.com>
2026-02-19 17:03:12 +0100

    fix: don't enforce SSH credentials from ssh_config content, Ansible's smart enough to pick the right ones per-host, if defined

--- kobra/ssh.go
+++ kobra/ssh.go
@@ -7,14 +7,10 @@
 package kobra
 
 import (
-	"net"
 	"os"
 	"os/user"
 	"path/filepath"
 	"strings"
-
-	"github.com/kevinburke/ssh_config"
-	"golang.org/x/crypto/ssh/agent"
 )
 
 const (
@@ -74,55 +70,13 @@ func GetSSHCredentials(ptfCfg *PlatformConfig, bootstrap bool) (string, string, 
 		key = expanded
 	}
 
-	if user == "" || key == "" {
-		// alternatively, try to find info in SSH config files ($HOME/.ssh/config or /etc/ssh/ssh_config)
-		hostCandidates := []string{
-			"10.*",
-			"192.168.*",
-			"*",
-		}
-		for _, host := range hostCandidates {
-			// check for a declared user
-			user = ssh_config.Get(host, SSHConfigUser)
-
-			// check for private key
-			key = ssh_config.Get(host, SSHConfigKey)
-			if key == "" {
-				// if no key has been set, try to find one from ssh-agent
-				authSock := os.Getenv(SSHAgentSocketEnv)
-				if authSock == "" {
-					continue
-				}
-
-				// connect to ssh-agent
-				sshAgent, err := net.Dial("unix", authSock) // #nosec G704
-				if err != nil {
-					continue
-				}
-
-				client := agent.NewClient(sshAgent)
-				keys, _ := client.List()
-				if len(keys) > 0 {
-					// don't know which to use if multiple are found, so pick first one
-					key = keys[0].Comment
-				}
-			}
-
-			if user != "" && key != "" {
-				break
-			}
-		}
-	}
-
-	if user != "" && key != "" {
+	if key != "" {
 		_, err := os.Stat(key)
 		if err != nil {
 			return user, key, KobraError("can't find SSH key file '%s'.", key)
 		}
-
-		// valid credentials have been found
-		return user, key, nil
 	}
 
-	return "", "", KobraError("no private SSH user/key settings can be found or guessed. Please check your platform configuration file")
+	// credentials have been found
+	return user, key, nil
 }
